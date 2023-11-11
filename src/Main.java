e75a2f5b5d573b948c31eb8bc02d9ff975426e11
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-11-11 12:49:59 -0300

    Coloquei o array que ela pediu

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[]{2, 8, 12, 7, 3};
+        int[] array = new int[]{49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28};
         /* Chamada Bubble
         array = Bubble.sort(array,array.length);
-         */
+        /*
+        Chamada Inseriton sort
         array = Insertion.Sort(array);
+        */
         System.out.println(Arrays.toString(array));
+
     }
 
 
